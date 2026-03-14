ef580396e0047f5589a2583c781db4af35926981
Zirwa Abid <zirwaabid112@gmail.com>
2026-03-14 11:55:15 +0500

    typescript course

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return "Hello" + name;
+}
+console.log(greet("Zirwa"));
+let username = "Zirwa Abid";
+let age = 21;
+let isDeveloper = true;
+console.log(username);
+console.log(age);
+console.log(isDeveloper);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+function greet(name:string){
+    return "Hello"+ name;
+}
+console.log(greet("Zirwa"));
+let username:string = "Zirwa Abid";
+let age:number=21;
+let isDeveloper:boolean=true;
+
+console.log(username)
+console.log(age)
+console.log(isDeveloper)
