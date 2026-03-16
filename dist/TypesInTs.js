41d64b32c2c8b1e1cc37af4c5e02e3e7fff0d5f2
Zirwa Abid <zirwaabid112@gmail.com>
2026-03-16 13:28:14 +0500

    typescript

--- dist/TypesInTs.js
+++ dist/TypesInTs.js
@@ -1,14 +1,79 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// inference ( in which we do not set data type , the ts automatically detect it)
+// ****inference ( in which we do not set data type , the ts automatically detect it)
 let name = "Zirwa";
 // name=22 error: type number is not assigneable to name because it is string so typescript is inference.
 let cups = Math.random() > 0.5 ? "10" : 5;
 // now ts understand that types of cups is string | number
-console.log(cups);
-// annotations
+// ***annotations
 let data = "zirwa";
 data = "developer";
 //  we assign the data type by ourself so this is annotation 
-console.log(data);
+// some other annotations:
+let value = "Zirwa";
+if (typeof value === "string") {
+    value = value.toUpperCase();
+    console.log(value);
+}
+function add(a, b) {
+    return a + b;
+}
+console.log(add(2, 5));
+let members = ["zirwa", "anas", "sara"];
+members.push("ahmed");
+console.log(members);
+let user1;
+user1 = {
+    name: "zirwa",
+    age: 21,
+    isPerson: true
+};
+console.log(user1);
+// >>>>>>Challenge 1
+// Fix the TypeScript types.
+function multiply(a, b) {
+    return a * b;
+}
+// >>>>Challenge 2
+// Create a variable status that only allows:
+// "loading"
+// "success"
+// "error"
+let status;
+//>>>> Challenge 3
+// Write a function:
+// isEven
+// Rules:
+// Input → number
+// Output → boolean
+// Example:
+// isEven(4) → true
+// isEven(7) → false
+function isEven(number) {
+    return number % 2 == 0;
+}
+console.log(isEven(3));
+// >>>>Challenge 4 (Intermediate)
+// Create this object type:
+// User
+// name → string
+// age → number
+// isAdmin → boolean
+// Then create a variable:
+// currentUser
+let currentUser;
+currentUser = {
+    name: "Zirwa",
+    age: 21,
+    isAdmin: true
+};
+function registerUser(user) {
+    return "User registered";
+}
+const use2 = {
+    name: "zirwa",
+    email: "zirwa@gmail.com",
+    age: 21
+};
+console.log(registerUser(use2));
 //# sourceMappingURL=TypesInTs.js.map
--- src/TypesInTs.ts
+++ src/TypesInTs.ts
@@ -2,10 +2,119 @@
 let name = "Zirwa";
 // name=22 error: type number is not assigneable to name because it is string so typescript is inference.
 
-let cups = Math.random() > 0.5 ? "10" : 5; 
+let cups = Math.random() > 0.5 ? "10" : 5;
 // now ts understand that types of cups is string | number
 
 // ***annotations
-let data:string="zirwa"; 
-data="developer";
+let data: string = "zirwa";
+data = "developer";
 //  we assign the data type by ourself so this is annotation 
+
+// some other annotations:
+let value: unknown = "Zirwa";
+if (typeof value === "string") {
+    value = value.toUpperCase();
+    console.log(value)
+}
+
+function add(a: number, b: number): number {
+    return a + b
+}
+console.log(add(2, 5))
+
+let members: string[] = ["zirwa", "anas", "sara"]
+members.push("ahmed");
+console.log(members)
+
+let user1: {
+    name: string,
+    age: number,
+    isPerson: boolean
+}
+user1 = {
+    name: "zirwa",
+    age: 21,
+    isPerson: true
+}
+console.log(user1)
+
+// >>>>>>Challenge 1
+
+// Fix the TypeScript types.
+
+function multiply(a: number, b: number): number {
+    return a * b
+}
+
+
+// >>>>Challenge 2
+
+// Create a variable status that only allows:
+
+// "loading"
+// "success"
+// "error"
+let status: "loading" | "success" | "error"
+
+
+//>>>> Challenge 3
+
+// Write a function:
+
+// isEven
+
+// Rules:
+
+// Input → number
+// Output → boolean
+
+// Example:
+
+// isEven(4) → true
+// isEven(7) → false
+function isEven(number: number): boolean {
+    return number % 2 == 0
+}
+console.log(isEven(3))
+
+// >>>>Challenge 4 (Intermediate)
+
+// Create this object type:
+
+// User
+// name → string
+// age → number
+// isAdmin → boolean
+
+// Then create a variable:
+
+// currentUser
+
+let currentUser: {
+    name: string,
+    age: number,
+    isAdmin: boolean
+}
+currentUser = {
+    name: "Zirwa",
+    age: 21,
+    isAdmin: true
+}
+
+
+// small project
+type user = {
+    name: string,
+    email: string,
+    age: number,
+}
+
+function registerUser(user: user): string {
+    return "User registered"
+}
+const user2 = {
+    name: "zirwa",
+    email: "zirwa@gmail.com",
+    age: 21
+}
+console.log(registerUser(user2))
